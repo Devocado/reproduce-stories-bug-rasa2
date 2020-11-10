7bf847a00a720dd155f1a25dcca3285df23034fb
jesse.duncan@xneelo.com <jesse.duncan@xneelo.com>
2020-11-10 12:34:53 +0200

    Change two-stage fallback to fallback

--- actions/actions.py
+++ actions/actions.py
@@ -59,15 +59,3 @@ class ShipItemForm(FormAction):
     ) -> List[Dict]:
 
         return []
-
-class DefaultFallbackActionOverride(Action):
-
-    def name(self) -> Text:
-        return "action_default_fallback"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="utter_fallback")
-
-        return []
